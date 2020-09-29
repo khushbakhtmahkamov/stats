30ac97abef3472b1af868b4b8a0086c74378a9f5
Khushbakht Mahkamov <khushbakhtmahkamov@gmail.com>
2020-09-29 08:24:25 +0500

    добавлен example test

---
+++ pkg/stats/example_test.go
@@ -0,0 +1,51 @@
+package stats
+
+import (
+	"fmt"
+	"github.com/khushbakhtmahkamov/bank/pkg/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			ID:       1,
+			Amount:   100,
+			Category: "qwerty",
+		},
+		{
+			ID:       2,
+			Amount:   200,
+			Category: "qwerty",
+		},
+		{
+			ID:       1,
+			Amount:   300,
+			Category: "qwerty",
+		},
+	}
+	fmt.Println(Avg(payments))
+	//Output: 200
+}
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			ID:       1,
+			Amount:   100,
+			Category: "kart",
+		},
+		{
+			ID:       2,
+			Amount:   200,
+			Category: "kart1",
+		},
+		{
+			ID:       1,
+			Amount:   300,
+			Category: "kart",
+		},
+	}
+	var category types.Category = "kart"
+	fmt.Println(TotalInCategory(payments,category))
+	//Output: 400
+}
